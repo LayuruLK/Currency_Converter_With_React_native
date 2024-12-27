92aef2eff29e58edb7b1c10634a7653cf857a87f
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-12-27 20:23:41 +0530

    Add State Variables in Converter

--- CurrencyConverter/components/Converter.js
+++ CurrencyConverter/components/Converter.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
-const Converter = () => (
-    <View>
-        <Text>Currency Converter Placeholder!</Text>
-    </View>
-);
+const Converter = () => {
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('EUR');
+    const [amount, setAmount] = useState('');
+    const [convertedAmount, setConvertedAmount] = useState(null);
+
+    return (
+        <View>
+            <Text>Currency Converter Placeholder!</Text>
+        </View>
+    );
+};
 
 export default Converter;
