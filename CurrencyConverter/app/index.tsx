455b4d851199407e22c5747721c2b599066cfa96
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-12-27 20:14:05 +0530

    add new component to develop the converter

--- CurrencyConverter/app/index.tsx
+++ CurrencyConverter/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import  Converter  from "../components/Converter"
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World</Text>
+      <Converter/>
     </View>
   );
 }
---
+++ CurrencyConverter/components/Converter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Converter = () => (
+    <View>
+        <Text>Currency Converter Placeholder</Text>
+    </View>
+);
+
+export default Converter;
